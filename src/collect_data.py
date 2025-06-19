2f243ed71966813d1611694c7afcee3a6cd9bd2c
son-dongwoo <sondongwoo2024@gmail.com>
2025-06-19 17:11:12 +0900

    refactor: clean code

--- src/collect_data.py
+++ src/collect_data.py
@@ -1,7 +1,7 @@
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'lerobot_mujoco_tutorial')))
 current_dir = os.path.dirname(__file__)
+sys.path.append(os.path.abspath(os.path.join(current_dir, '..', 'lerobot_mujoco_tutorial')))
 
 import numpy as np
 from PIL import Image
